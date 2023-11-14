10817fa5e236e463bfbebe618bf9d2c11abd971b
EverythingIsCodeCodeCode <github1.20.thesoundman@neverbox.com>
2023-11-13 21:54:08 -0600

    initial commit

---
+++ split_files_into_new_folders.ps1
@@ -0,0 +1,21 @@
+<#
+2023-11-13 Mon. 9p.
+This script will split files in a folder into new folders so that each folder contains the same number of files except the last folder.
+This is useful for things like a folder of many audio files.  Some audio players only recognize up to a certain number of files in each folder.
+
+rough draft of notes:
+define variables
+source folder
+target folder to make new folders in
+number of files in each new folder
+name of new folders
+numbering of new folders
+
+main loop to check for files in target folder
+do you just want one loop?  can this be done in a single loop?
+
+#>
+
+
+
+#End of script.
