b250dbc5d1d24eca9af0c551ecd728de197f3ef5
EverythingIsCodeCodeCode <github1.20.thesoundman@neverbox.com>
2024-01-09 09:18:57 -0600

    Updated help notes.

--- Sean_help001.ps1
+++ Sean_help001.ps1
@@ -8,6 +8,29 @@ This is some stuff Sean has shown me.
 $ports="2181","2888","3182"
 foreach ($port in $ports) { $i = $ports.IndexOf($port) ; $i}
 
+<#
+2024-01-09 Tuesday 8:30a.
+Here's one way that may be faster to show a folder's size than Get-ChildItem.
+.Net/COM FileSystemObject method:
+#>
+$FSO = New-Object -ComObject Scripting.FileSystemObject -ErrorAction Stop
+$folder = Get-Item -LiteralPath "C:\temp"
+$FolderSize = $FSO.GetFolder($folder.FullName).Size
+$FolderSizeBytes = "{0:N2}" -f ([int64]$FolderSize)
+$FolderSizeMB = "{0:N2}" -f ([int64]$FolderSize / 1MB)
+$FolderSizeGB = "{0:N2}" -f ([int64]$FolderSize / 1GB)
+
+<#
+2024-01-09 Tuesday 8:43a.
+Here's one way that may be faster to show a folder's size than Get-ChildItem.
+Robocopy Method:
+#>
+[regex]$BytesLineRegex = 'Bytes\s*:\s*(?<ByteCount>\d+)(?:\s+\d+){3}\s+(?<BytesFailed>\d+)\s+\d+'
+$FolderSize = Robocopy.exe $folder.FullName NULL /L /S /NJH /BYTES /FP /NC /TS /XJ /R:0 /W:0 /MT:16 | Select-Object -Last 4
+$FolderSize -match "$BytesLineRegex" | Out-Null
+$FolderSizeBytes = "{0:N2}" -f ([int64]$Matches.ByteCount)
+$FolderSizeMB = "{0:N2}" -f ([int64]$Matches.ByteCount / 1MB)
+$FolderSizeGB = "{0:N2}" -f ([int64]$Matches.ByteCount / 1GB)
 
 #End of file.
 
