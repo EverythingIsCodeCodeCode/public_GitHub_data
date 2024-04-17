3319d11879463b80926580111146dfba2f771833
EverythingIsCodeCodeCode <github1.20.thesoundman@neverbox.com>
2024-04-17 15:15:43 -0500

    Added more text to output.

--- random_number_generator.ps1
+++ random_number_generator.ps1
@@ -12,8 +12,31 @@ Finally, the shuffled numbers are displayed. Feel free to adjust the range as ne
 #>
 
 # Define the range (inclusive)
-$Minimum = 1
-$Maximum = 10
+#$Minimum = 1
+#Maximum = 10
+Write-Output -InputObject ""
+Write-Output -InputObject "This PowerShell script will ask you for the smallest and largest raffle ticket numbers then draw them at random."
+Write-Output -InputObject ""
+$Minimum = Read-Host -Prompt "What is the smallest ticket number?"
+$Maximum = Read-Host -Prompt "What is the largest ticket number?"
+Write-Output -InputObject ""
+
+if (Test-Path "/home") {
+	# Remote machine is Linux
+	$PathSeparator = "/"
+}
+elseif (Test-Path "/Users") {
+	# Remote machine is macOS
+	$PathSeparator = "/"
+}
+elseif (Test-Path "C:\Users") {
+	# Remote machine is Windows
+	$PathSeparator = "\"
+}
+else {
+	Write-Error "Could not determine the remote operating system."
+	return
+}
 
 # Create an array of numbers within the range
 $NumberRange = $Minimum..$Maximum
@@ -22,15 +45,21 @@ $NumberRange = $Minimum..$Maximum
 $ShuffledNumbers = $NumberRange | Get-Random -Count $NumberRange.Count
 
 # Output the shuffled numbers
+Write-Output -InputObject "Here are the numbers in random order:"
 $ShuffledNumbers
-Pause
-#clear # it removes the bulk output 
 
 # Variables for counting and controlling the loop
 $CountUp = 0
 $totalItems = $ShuffledNumbers.Count
+Write-Output -InputObject ""
+Write-Output -InputObject "That's a total of $totalItems numbers."
+Write-Output -InputObject ""
+Write-Output -InputObject "Now, let's show them one at a time."
+Write-Output -InputObject ""
 
 $range = $Maximum - $Minimum
+Pause
+#clear # it removes the bulk output 
 # Loop through and output the shuffled numbers one at a time:
 foreach ($CurrentLoopItem in $ShuffledNumbers)
 {
@@ -47,3 +76,8 @@ foreach ($CurrentLoopItem in $ShuffledNumbers)
 		Pause
 	}
 }
+
+Write-Output -InputObject ""
+Write-Output -InputObject "All done."
+Write-Output -InputObject ""
+
