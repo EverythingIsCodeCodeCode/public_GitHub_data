7a559eb8a10e08071b40d8268594c551cbdd0f87
EverythingIsCodeCodeCode <github1.20.thesoundman@neverbox.com>
2024-12-01 19:31:22 -0600

    Initial commit.

---
+++ Singspiration.ps1
@@ -0,0 +1,44 @@
+<#
+2024-12-01sp.
+This script will help with figuring out Singspiration dates which usually take place on the 5th Sunday night at 6p in a given month.
+
+You may need to figure out how many days are in each month in addition to counting the number of Sundays in each month.
+You need to figure out if Easter takes place the same time as Singspiration so you can skip the Singspiration.
+Is there a way to get all the Easter dates from somewhere or just load them in manually?
+We've only had 1 Singspiration in December in 2013 so we may just skip Singspiration in December because of Christmas & New Years.
+
+Count the number of weeks for any given service until the next Singspiration.
+Count the number of weeks remaining to signup for the next Singspiration.
+
+Remember, you need approximately 2 weeks to comfortability process everything before Singspiration::
+sa - can signup for current event
+sp - can signup for next event
+wp - can signup for next event
+sa - can signup for next event
+sp - can signup for next event
+wp - can signup for next event
+sa - can signup for next event
+sp - event takes place
+
+Might borrow code that gets church services for next year.
+
+https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7.3
+https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings?view=netframework-4.8
+#>
+
+# Import variables using dot sourcing:
+. .\gitignore_data\variables.ps1
+# List variables:
+# Ok, no variables to use/list just yet.
+
+$StartDate = Get-Date -Year ((Get-Date).year+1) -Month 01 -Day 01 # This gets the first day of next year.
+$EndDate = Get-Date -Year ((Get-Date).year+1) -Month 12 -Day 31 # This gets the last day of next year.
+$Jan = Get-Date -Year ((Get-Date).year+1) -Month 1 # This gets January next year.
+
+
+While ($StartDate -lt $EndDate) # This starts a while loop for the year.
+{
+
+	$StartDate = $StartDate.AddDays(1)
+}
+
