444df9c59fc84474f163f01269662d89063eba26
EverythingIsCodeCodeCode <github1.20.thesoundman@neverbox.com>
2024-12-04 20:05:23 -0600

    Added Easter website to comments.

--- Singspiration.ps1
+++ Singspiration.ps1
@@ -5,6 +5,7 @@ This script will help with figuring out Singspiration dates which usually take p
 You may need to figure out how many days are in each month in addition to counting the number of Sundays in each month.
 You need to figure out if Easter takes place the same time as Singspiration so you can skip the Singspiration.
 Is there a way to get all the Easter dates from somewhere or just load them in manually?
+https://www.timeanddate.com/calendar/determining-easter-date.html
 We've only had 1 Singspiration in December in 2013 so we may just skip Singspiration in December because of Christmas & New Years.
 
 Count the number of weeks for any given service until the next Singspiration.
